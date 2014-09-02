e5f752cfa9fa1e95935677bdb1344571bfb23710
Utilisateur6 <dominique.c@sport-decouverte.com>
2014-09-02 16:24:54 +0200

    prise en compte des données static table example

---
+++ Data/dbo.Examples_Data.sql
@@ -0,0 +1,5 @@
+SET IDENTITY_INSERT [dbo].[Examples] ON
+INSERT INTO [dbo].[Examples] ([ExampleId], [ExampleName]) VALUES (1, N'TCP')
+INSERT INTO [dbo].[Examples] ([ExampleId], [ExampleName]) VALUES (2, N'WIFI')
+INSERT INTO [dbo].[Examples] ([ExampleId], [ExampleName]) VALUES (3, N'BlueTooth')
+SET IDENTITY_INSERT [dbo].[Examples] OFF
